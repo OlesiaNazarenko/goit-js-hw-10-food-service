11d85893d6f28ce9817444d491d837ddbfc6e5e5
OlesiaNazarenko <eightpa8@gmail.com>
2021-08-30 13:39:47 +0300

    Добавила сохранение темы.

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,5 @@
 import menuTemplate from '../templates/menu.hbs';
 import menu from './menu.json';
-import Theme from './theme';
 
 const menuMarkup = document.querySelector('.js-menu');
 const cardMarkup = createMenuMarkup(menu);
@@ -11,58 +10,33 @@ function createMenuMarkup(menu) {
 const body = document.querySelector('body');
 const toolbox = document.querySelector("#theme-switch-toggle");
 
-// function changeOnDarkTheme(evt) {
-    
-//     if (evt.target.classList.contains("dark-theme")) {
-//         evt.target.setAttribute('checked', 'true');
-        
-//     }
-//     evt.target.setAttribute('checked', 'false');
-//      localStorage.removeItem("class", "dark-theme");
-// }
-// let theme = localStorage.getItem("class");
-// body.classList.add(theme);
-toolbox.addEventListener("change", (e) => {
-    body.classList.toggle("dark-theme")
-    console.log(localStorage.getItem("class"))
-    if (localStorage.getItem("class")) {
-        localStorage.removeItem("class")
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+const { LIGHT, DARK } = Theme;
+
+function setThemeName(themeName) {
+    localStorage.setItem('theme', themeName);
+    body.className = themeName;
+}
+function toggleTheme() {
+    if (localStorage.getItem('theme') === LIGHT) {
+        setThemeName(DARK);
     } else {
-        localStorage.setItem("class", "dark-theme")
-        toolbox.checked = true;
+        setThemeName(LIGHT);
     }
-});
+}
+function saveTheme() {
+    if (localStorage.getItem('theme') === DARK) {
+        setThemeName(DARK);
+        toolbox.checked = true;
+    } else {
+        setThemeName(LIGHT);
+        toolbox.checked = false;
+    }
+};
 
-
-
-
-// function replaceSelectorsOnBodyRef() {
-    
-//     if (body.classList.contains(Theme.LIGHT)) {
-//         body.classList.replace(Theme.LIGHT, Theme.DARK);
-//         saveUserThemeInLocalStorage(Theme.DARK);
-//         console.log("fhfjjf")
-//         return;
-//     }
-//     body.classList.replace(Theme.DARK, Theme.LIGHT);
-//     saveUserThemeInLocalStorage(Theme.LIGHT);
-// }
-// function onThemeChange() {
-//     replaceSelectorsOnBodyRef();
-// }
-
-// function saveUserThemeInLocalStorage(theme) {
-//     localStorage.setItem('theme', theme);
-// }
-
-// function saveUserThemeWhenPageRefresh() {
-//     const userTheme = localStorage.getItem('theme');
-
-//     if (userTheme === Theme.DARK) {
-//         document.body.classList.replace(Theme.LIGHT, Theme.DARK);
-//         toolbox.checked = true;
-//     }
-// }
-// saveUserThemeWhenPageRefresh();
-
-// toolbox.addEventListener('change', onThemeChange);
+saveTheme();
+toolbox.addEventListener('change', toggleTheme); 
--- src/js/theme.js
+++
@@ -1,5 +0,0 @@
-const theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-export default theme;
